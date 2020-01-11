f5f8c6a15d53ded5e3da48aca9deed6ae0e2dff9
Fernando García <ferisagaragu@gmail.com>
2020-01-11 14:46:19 -0600

    Daos

--- src/main/java/org/neurobrain/tlozbotw/dao/IRoleDAO.java
+++ src/main/java/org/neurobrain/tlozbotw/dao/IRoleDAO.java
@@ -1,9 +1,9 @@
 package org.neurobrain.tlozbotw.dao;
 
 import java.util.Optional;
-
 import org.neurobrain.tlozbotw.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 
 public interface IRoleDAO extends JpaRepository<Role, Long> {
 	Optional<Role> findByName(String name);
